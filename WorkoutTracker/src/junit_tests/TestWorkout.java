6a1d6249c826585483055a274f7e8bf7d9427459
abeattie077 <abeattie077@gmail.com>
2026-05-13 18:34:03 -0400

    Workout Tracker File Setups

---
+++ WorkoutTracker/src/junit_tests/TestWorkout.java
@@ -0,0 +1,14 @@
+package junit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestWorkout {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
