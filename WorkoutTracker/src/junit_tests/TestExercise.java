1d06ba80df85dcd8d1015ae729ec7b7123f88b9a
abeattie077 <abeattie077@gmail.com>
2026-05-15 22:02:02 -0400

    Lab 2 Work

--- WorkoutTracker/src/junit_tests/TestExercise.java
+++ WorkoutTracker/src/junit_tests/TestExercise.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.*;
+
 public class TestExercise {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test01() {
+		Exercise benchPress = new Exercise();
+		benchPress.setName("   Bench Press        ");
+		benchPress.setDescription("  This exercise tagets the chest and shoulders              ");
+		assertEquals("Bench Press", benchPress.getName());
+		assertEquals("This exercise tagets the chest and shoulders", benchPress.getDescription());
 	}
 
 }
--- WorkoutTracker/src/model/Exercise.java
+++ WorkoutTracker/src/model/Exercise.java
@@ -1,5 +1,46 @@
 package model;
 
 public class Exercise {
+	//Attributes
+	private String name;
+	private String description;
+	private int reps;
+	private double weightLBS;
+	private double weightKG;
+	
+	//Constructors
+	public Exercise() {
+		this.name = "";
+		this.description = "";
+	}
+	public Exercise (String name, String description, int reps, double weight, boolean isLBS) {
+		setName(name);
+		setDescription(description);
+	}
+	
+	//Getters
+	public String getName() {
+		return this.name;
+	}
+	public String getDescription() {
+		return this.description;
+	}
 
+	//Setters
+	public void setName(String name) {
+		if (name == null && name.strip().isEmpty()) {
+			throw new IllegalArgumentException("The name cannot be empty.");	
+		}
+		else {
+			this.name = name.strip();
+		}
+	}
+	public void setDescription(String description) {
+		if (description == null && description.strip().isEmpty()) {
+			throw new IllegalArgumentException("The description cannot be empty");	
+		}
+		else {
+			this.description = description.strip();
+		}
+	}
 }
